bfcecf80b1ea7d80bfc57120d659a760aa4f3bb8
gizeasy <gizeasy@gmail.com>
2022-09-15 12:58:53 +0300

    Update rcTreeAdapter.stand.tsx

--- src/adapters/rcTreeAdapter/__stand__/rcTreeAdapter.stand.tsx
+++ src/adapters/rcTreeAdapter/__stand__/rcTreeAdapter.stand.tsx
@@ -1,11 +1,8 @@
 import { createStand } from '##/stand/standConfig';
 
-import image from './rcTreeAdapter.image.svg';
-
 export default createStand({
   title: 'rcTreeAdapter',
   id: 'rcTreeAdapter',
-  image,
   group: 'adapters',
   description: 'Адаптер для стилизации дерева.',
   version: '1.0.8',
